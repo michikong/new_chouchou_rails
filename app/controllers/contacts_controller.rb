f7a1d6022e29280acefc9b1cf6a18c1dd067a660
michinobu.sekikawa <abc_0615@icloud.com>
2019-09-20 20:57:01 +0900

    index,show,edit,creageを追加

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,20 +4,39 @@ class ContactsController < ApplicationController
    end
 
    def show
+      @contact = Contact.find(params[:id])
    end
 
    def new
+      @contact = Contact.new
    end
 
    def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+         flash[:success] = 'Contact が正常に投稿されました'
+         redirect_to @contact
+      else
+         flash.now[:danger] = 'Contact が投稿されませんでした'
+         render :new
+      end
    end
 
    def edit
+      @contact = Contact.find(params[:id])
    end
 
    def update
    end
 
    def destroy
+   end
+   
+   # 以降に定義されたメソッドがこのクラス内でのみ使用することを明示
+   private
+   
+   # Strong Parameter
+   def contact_params
+      params.require(:contact).permit(:contact)
    end
 end
